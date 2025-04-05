e97d9c85e0fee1fa525700911a4ffde84a66399f
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-05 23:01:56 +0530

    Updated features and fixed bugs

--- src/app/client/client-routing.module.ts
+++ src/app/client/client-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ClientComponent } from './client.component';
 import { ClientDashboardComponent } from './pages/client-dashboard/client-dashboard.component';
 import { AdDetailComponent } from './pages/ad-detail/ad-detail.component';
 import { MyBookingsComponent } from './pages/my-bookings/my-bookings.component';
@@ -9,7 +8,6 @@ import { ReviewComponent } from './pages/review/review.component';
 const routes: Routes = [
   { path: 'dashboard', component: ClientDashboardComponent },
   { path: 'bookings', component: MyBookingsComponent },
-
   { path: 'ad/:adId', component: AdDetailComponent },
   { path: 'review/:id', component: ReviewComponent } 
 ];
